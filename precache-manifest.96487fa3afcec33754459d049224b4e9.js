d4b5e01fdfb9313f34d181f6160ff9005885f8f0
Mark Kadiner <dasistmail765@gmail.com>
2021-04-20 23:52:21 +0300

    Updates

---
+++ precache-manifest.96487fa3afcec33754459d049224b4e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67450f762f23cfe358e99e5b066d3978",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4162e1f0c565bedd54e",
+    "url": "/static/js/2.5266f0b7.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.5266f0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e003074c198ac48c6250",
+    "url": "/static/js/main.a1a972fd.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
